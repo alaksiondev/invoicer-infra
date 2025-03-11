d03ecb33e148138074f4e23500b78f45ad0fb1ec
Alaksion <lbeurmann.dev@gmail.com>
2025-03-11 01:34:22 -0300

    feat: update sql scripts

--- sql/07-qrcode-token.sql
+++ sql/07-qrcode-token.sql
@@ -2,7 +2,10 @@ CREATE TABLE IF NOT EXISTS t_qrcode_tokens(
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     ip_address varchar not null,
     agent varchar not null,
+    base64_content varchar not null,
+    content varchar not null unique,
+    expires_at TIMESTAMP not null,
     created_at TIMESTAMP default CURRENT_DATE,
     updated_at TIMESTAMP default CURRENT_DATE,
-    enabled boolean not null default true,
-)
+    status varchar check (status in ('generated', 'consumed', 'expired'))
+);
